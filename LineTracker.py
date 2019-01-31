1dd622542f190d73bf31cd7ad6e12ad0d4017274
DLPMSR-PEACH\Zechs <pntsec_msr@outlook.com>
2019-02-01 02:56:25 +0900

    多分動く

--- LineTracker.py
+++ LineTracker.py
@@ -1,33 +1,39 @@
+import sys
+import os
 import numpy as np
 import cv2
 import csv
 from tqdm import tqdm
 
-from ColorRopeDetector import ColorRopeDetector
-from ColorRopeDetector import ImageProcessing
+from Components import DetectionProcessing
+from Components import ImageProcessing
+
 
 def lineTracker(video_path):
     cap = cv2.VideoCapture(video_path)
+    filename = video_path[:-4]
     pbar = tqdm(total=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi',fourcc, 30.0, (1440,1080))
-    f = open('./output.csv', 'w')
+    out = cv2.VideoWriter('{}_output.avi'.format(filename),fourcc, 30.0, (1440,1080))
+    f = open('{}_output.csv'.format(filename), 'w')
     writer = csv.writer(f, lineterminator='\n')
-    writer.writerow(['frame', 'right','left'])
+    writer.writerow(['num', 'L_pix', 'R_pix', 'L_mm','R_mm'])
     result_list = []
-    frame_num = 0
+    num_frame = 0
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret==True:
-            frame = ImageProcessing(frame)
-            frame_preprocessed = frame.get_preprocessed_image()
-            frame_status = ColorRopeDetector(frame_preprocessed, frame_num)
-            out.write(frame.get_output_image())
-            result_list.append(frame_status.get_result())
-            cv2.namedWindow('frame', cv2.WINDOW_KEEPRATIO | cv2.WINDOW_NORMAL)
-            cv2.imshow('frame',frame.get_output_image())
-            frame_num += 1
+            img = ImageProcessing(frame)
+            img_preprocessed = img.get_preprocessed_image()
+            frame_status = DetectionProcessing(img_preprocessed)
+            img_output = img.get_output_image(frame_status)
+            out.write(img_output)
+            if frame_status.is_detected:
+                result_list.append([num_frame]+frame_status.get_result())
+            num_frame += 1
             pbar.update(1)
+            cv2.namedWindow('frame', cv2.WINDOW_KEEPRATIO | cv2.WINDOW_NORMAL)
+            cv2.imshow('frame', img_output)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
@@ -41,9 +47,10 @@ def lineTracker(video_path):
     cv2.destroyAllWindows()
 
 
-def main():
-    lineTracker('./00011.MTS')
+def main(arg):
+    lineTracker(arg)
 
 
 if __name__ == '__main__':
-    main()  
+    args = sys.argv
+    main(args[1])  
